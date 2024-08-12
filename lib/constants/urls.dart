1a2542478f46e06d40e7cce422c118f2fbbd5c22
Sai Kumar Reddy Baddala <saibaddala172@gmail.com>
2024-08-12 21:08:54 +0530

    Delete lib directory

--- lib/constants/urls.dart
+++
@@ -1,2 +0,0 @@
-const webHost = "http://localhost:3001";
-const emulatorHost = "http://10.0.2.2:8000";
--- lib/models/response_model.dart
+++
@@ -1,9 +0,0 @@
-class ResponseModel {
-  final String? errorMsg;
-  final dynamic data;
-
-  ResponseModel({
-    required this.errorMsg,
-    required this.data,
-  });
-}
